26ab0ffd9d673403eb474b7a0c8a42591d5b244b
Saidi <rasheedsaidi@gmail.com>
2018-04-21 22:13:44 +0100

    Complete Board.js and Box.js draft

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,50 +1,204 @@
 import React, { Component } from 'react';
 import Box from './Box';
-import TodoList from './TodoList';
 
 class Board extends Component {
+    
     constructor (props) {
-        const WIDTH = 5;
-        const HEIGHT = 3;
+        super(props);
+        const TEN = 10;
+        this.UP = 38;
+        this.DOWN = 40;
+        this.LEFT = 37;
+        this.RIGHT = 39;
+        let x, y, matrix, balls;
+        
+        this.WIDTH = parseInt(prompt("Enter board width"));
+        this.HEIGHT = parseInt(prompt("Enter board height"));
+
+        if (null === this.WIDTH || isNaN(this.WIDTH) || this.WIDTH < 3) 
+            this.WIDTH = TEN;
+
+        if (null === this.HEIGHT || isNaN(this.HEIGHT) || this.HEIGHT < 3) 
+            this.HEIGHT = TEN;
+
+        this.matrix = this.fillList(this.WIDTH, this.HEIGHT);
+        this.state = {
+            row: this.WIDTH,
+            col: this.HEIGHT,
+            moves: 0,
+            balls: this.balls,
+            doll: {x: this.x, y: this.y},
+            matrix: this.matrix
+        }
+        
+        this.onKeyPress = this.onKeyPress.bind(this);
     }
+
     render() {
         return (
             <div className="game">
-            <div className="board">
-                <div className="board-row">
-                    <Box mode={0} />
-                    <Box mode={1} />
-                    <Box />
+                <div className="board">
+                    {this.buildBoard()}
                 </div>
-                <div className="board-row">
-                    <Box />
-                    <Box mode={2} />
-                    <Box />
-                </div>
-                <div className="board-row">
-                    <TodoList />
-                </div>
-            </div>
             </div>
         );
     }
 
-    buildBoard (width, height) {
-        let matrix = getArrangement(width, height);
-        for(let i = 0; i < height; i++) {
-            let board_row = '';
-            for(let j = 0; j < width; j++) {
-                board_row += <Box x={i} y={j} mode={matric[i][j]} />
+    onKeyPress(event) { 
+        let key = event.keyCode;
+        if(key === this.LEFT || key === this.UP || key === this.RIGHT || key === this.DOWN) {
+            let matrix = this.state.matrix.slice(); 
+            let row = this.state.doll;
+            matrix[row.x][row.y].mode = 0;
+            let key_id = row.x + '_' + row.y;
+            let doll;
+            let current_x;
+            let current_y;
+            let y;
+            let x;
+            switch(key) {
+                case this.UP:
+                    current_x = row.x;
+                    y = row.y;
+                    x = current_x - 1;
+                    doll = {x: x, y: y};                    
+                    break;
+                case this.DOWN:
+                    current_x = row.x;
+                    y = row.y;
+                    x = current_x + 1;
+                    doll = {x: x, y: y};                    
+                    break;
+                case this.RIGHT:
+                    current_y = row.y;
+                    x = row.x;
+                    y = current_y + 1;
+                    doll = {x: x, y: y};                    
+                    break;
+                case this.LEFT:
+                    current_y = row.y;
+                    x = row.x;
+                    y = current_y - 1;
+                    doll = {x: x, y: y};                    
+                    break;
+                default:                    
+                    break;
             }
-        }
+
+            if(this.isValidLocation(doll)) { 
+                if(matrix[doll.x][doll.y]) {
+                    if(this.hasBall(doll))
+                        this.setState(...this.state, {balls: this.state.balls - 1});
+                    matrix[doll.x][doll.y].mode = 2;
+                    this.setState({matrix: matrix});
+                    this.setState({doll: doll});
+                    this.setState(...this.state, {moves: this.state.moves + 1});
+                    
+    
+                    if(this.gameIsComplete()) {
+                        alert("You saved our darling princess by " + this.state.moves + " moves.");
+                        window.location.reload();
+                    }
+                }
+                
+            }
+        } 
     }
 
-    getArrangement(width, height) {
-        matrix = [];
-        for(let i = 0; i < height; i++) {
+    gameIsComplete() {
+        return this.state.balls === 0;
+    }
+
+    hasBall(loc) {
+        let matrix = this.state.matrix;
+        return matrix[loc.x][loc.y].mode === 1;
+    } 
+
+    isValidLocation(doll) {
+        return (doll.x >= 0 && doll.x < this.HEIGHT) && (doll.y >= 0 && doll.y < this.WIDTH); 
+    }
+
+    componentWillMount() {
+		document.addEventListener("keydown", this.onKeyPress);
+    }    
+
+    buildBoard() {
+        let board = [];
+        for(let i in this.state.matrix) {
+            let board_row = [];
+            let row = this.state.matrix[i];
+            for(let j in row) {
+                       
+                let dstate = this.state.matrix[i][j];
+                let k = i + '_' + j;
+                board_row.push( <Box key={k} id={this.state.matrix[i][j].key} x={this.state.matrix[i][j].x} y={this.state.matrix[i][j].y} mode={this.state.matrix[i][j].mode} /> );
+                
+            }
+            board.push(<div key={i} className="board-row">
+                { board_row }
+            </div>);
+        }
+        return board;
+    }
+
+    getArrangement(row, col) {
+        let matrix = [];
+        for(let i = 0; i < row; i++) {
             matrix[i] = [];
-            for(let j = 0; j < width; j++) {
+            for(let j = 0; j < col; j++) {
                 matrix[i][j] = 0;
+            }
+        }
+        return matrix;
+    }
+
+    getDummyList(row, col) {
+        let list = [];
+        let pos = Math.floor(Math.random() * row * col);
+        for(let i = 0; i < row * col; i++) {
+            list[i] = 0;
+        }
+        list[Math.floor(Math.random() * list.length)] = 2;
+        return list;
+    }
+
+    fillList(row, col) {
+        let list = this.getDummyList(row, col);
+        let fittings = Math.floor(Math.floor(row * col * .70));
+        this.balls = fittings;
+        let k = 0;
+        while (true) {
+            let i = Math.floor(Math.random() * row * col);             
+            if(typeof list[i] !== 'undefined' && list[i] === 0) {
+                list[i] = 1;
+                k += 1;
+                if(k >= fittings){
+                    break;
+                }               
+            }
+        }
+        return this.reshape(list, row);
+    }
+
+    reshape(list, row) {
+        let matrix = [];
+        let x = 0;
+        let y = 0;
+        let line = [];
+        for(let i = 0; i < list.length; i++) {
+            let k = x + '_' + y;
+            line[y] = {x: x, y: y, mode: list[i], key: k};
+            if(list[i] === 2) {
+                this.x = x;
+                this.y = y;
+            }
+            y += 1;
+            if (line.length === row) {
+                matrix[x] = line;
+                
+                line = [];
+                x += 1; 
+                y = 0;               
             }
         }
         return matrix;
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,48 +1,80 @@
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import {TimelineLite} from 'gsap'
 
 class Box extends React.Component {
     constructor (props) {
         super(props);
-        this.handleKeyDown = this.handleKeyDown.bind(this);
-        this.state = {
-            mode: this.props.mode
-        }
-    }
 
+        this.state = {
+            KeyPress: this.props.KeyPress,
+            mode: this.props.mode,
+            x: this.props.x,
+            y: this.props.y,
+        }
+
+    }
     render() {
-        let content = this.setContent(this.state.mode)
+        //let content = 
         return (
-            
-            <a href="#" onKeyDown={this.handleKeyDown} className="box" ref={dol => this.dol = dol}>
-                {content}
-            </a>
+            <button id={this.props.id} key={this.props.id} x={this.props.x} y={this.props.y} mode={this.props.mode} className="box">
+                {this.setContent(this.props.mode)}
+            </button>
         );
     }
 
+    movement(key, a) {
+
+        this.props.KeyPress(key, this.props);
+        
+        console.log(this.state)
+        let dull_pos = this.state.doll;
+        let id = dull_pos.x + '_' + dull_pos.y;
+        console.log(this.state.matrix[dull_pos.x][dull_pos.y], document.getElementById(id), id);
+        let matrix = this.state.matrix.slice();
+        let row = matrix[dull_pos.x][dull_pos.y];
+        matrix[dull_pos.x][dull_pos.y].mode = 1;
+        let key_id = row.key;
+        let loc = document.getElementById(key_id);
+        this.setState(...this.state, {matrix: matrix});
+        switch(key) {
+            case this.UP:
+                let current_x = row.x;
+                let current_y = row.y;
+
+                let y = current_y - 1;
+                this.setState(...this.state, {doll: {x: current_x, y: y}});
+                matrix[dull_pos.x][dull_pos.y].mode = 2;
+                this.setState(...this.state, {matrix: matrix});
+                id = current_x + '_' + y;
+                loc = document.getElementById(id);
+                let doll = 
+                loc.innerHTML = '';
+            
+        }
+    }
+
+    onKeyPress(event, props){ console.log(this.state, event)
+		if(event.keyCode === this.LEFT || event.keyCode === this.UP || event.keyCode === this.RIGHT || event.keyCode === this.DOWN) {
+			
+			//this.setState(...this.state, {moves: this.state.moves + 1});
+
+        }
+        this.props.KeyPress(event.keyCode, this.state);
+	}	
+
     moveLeft = () => {
-        var animation = new TimelineLite()
-        animation
-          .to(this.dol, 1, { x: -25 })
+       
     }
 
     moveRight = () => {
-        var animation = new TimelineLite()
-        animation
-          .to(this.doll, 1, { x: 25 })
+       
     }
 
     moveUp = () => {
-        var animation = new TimelineLite()
-        animation
-          .to(this.doll, 1, { y: -25 })
+        
     }
 
     moveDown = () => {
-        var animation = new TimelineLite()
-        animation
-          .to(this.doll, 1, { y: 25 })
+        
     }
 
     handleKeyDown(e) {
@@ -76,7 +108,7 @@ class Box extends React.Component {
                 return <img className="item-img" src={require('../images/ball.png')} />;
                 break;
             case 2:
-                return <img className="item-img doll" onKeyDown={this.handleKeyDown} onClick={this.moveLeft} ref={doll => this.doll = doll} src={require('../images/doll.png')} />;
+                return <img className="item-img doll" src={require('../images/doll.png')} />;
                 break;
             default:
                 return '';
